85948d9685317a46203dc69b7d42cc32bccd1f92
Htoo Pyae <htoopyae.dev@gmail.com>
2024-01-02 14:06:39 +0700

    feat: path and cli flag

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("Welcome to GoGitGraph!")
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var path string
+	flag.StringVar(&path, "p", currentDir, "directory to cacluate stats for")
+	flag.Parse()
+
+	basePath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Welcome to GoGitGraph!, your chosen path: ", basePath)
 }
