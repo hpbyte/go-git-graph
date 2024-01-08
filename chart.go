af327be3754dbb74e740a9511a5d94d1ca52a22c
Htoo Pyae <htoopyae.dev@gmail.com>
2024-01-09 00:00:43 +0700

    feat: updated chart

--- chart.go
+++ chart.go
@@ -3,22 +3,21 @@ package main
 import (
 	"fmt"
 	"log"
-	"strconv"
 	"time"
 )
 
-type Color string
+const layout = "2006-01-02"
+const numOfDaysInWeek = 7
+const numOfweeksInYear = 53
 
 const (
-	ColorBlack  Color = "\u001b[30m"
-	ColorRed          = "\u001b[31m"
-	ColorGreen        = "\u001b[32m"
-	ColorYellow       = "\u001b[33m"
-	ColorBlue         = "\u001b[34m"
-	ColorReset        = "\u001b[0m"
+	blankColor       = "\033[48;5;238m"
+	dimGreenColor    = "\033[48;5;22m"
+	midGreenColor    = "\033[48;5;28m"
+	brightGreenColor = "\033[48;5;34m"
+	resetColor       = "\033[0m"
+	highlightColor   = "\033[48;5;226m"
 )
-
-const layout = "2006-01-02"
 
 type Chart interface {
 	Render()
@@ -31,7 +30,7 @@ type ContributionChart struct {
 
 func (cc ContributionChart) Render() {
 	// 7 (days) x 53 (weeks) grid 0s for a year with 0s as default
-	var grid [7][53]int
+	var grid [numOfDaysInWeek][numOfweeksInYear]int
 
 	for commitDate, count := range cc.Data {
 		parsed, err := time.Parse(layout, commitDate)
@@ -48,14 +47,63 @@ func (cc ContributionChart) Render() {
 		}
 	}
 
-	for _, row := range grid {
+	printMonths()
+	for i, row := range grid {
+		printDayCol(i)
 		for _, col := range row {
-			cc.colorizer(strconv.Itoa(col), ColorBlue)
+			printCell(col, false)
 		}
 		fmt.Println()
 	}
 }
 
-func (cc ContributionChart) colorizer(message string, color Color) {
-	fmt.Print(string(color), message, string(ColorReset))
+func printMonths() {
+	var months [53]string
+	offset := 0
+	for month := time.January; month <= time.December; month++ {
+		months[offset] = month.String()[:3]
+		offset += 4
+	}
+
+	fmt.Print("     ")
+	for _, month := range months {
+		fmt.Printf("%s ", month)
+	}
+	fmt.Println()
+}
+
+func printDayCol(day int) {
+	out := "     "
+	switch day {
+	case 1:
+		out = " Mon "
+	case 3:
+		out = " Wed "
+	case 5:
+		out = " Fri "
+	}
+
+	fmt.Printf(out)
+}
+
+func printCell(count int, today bool) {
+	var colorCode string
+
+	switch {
+	case count == 0:
+		colorCode = blankColor
+	case count <= 5:
+		colorCode = dimGreenColor
+	case count <= 10:
+		colorCode = midGreenColor
+	default:
+		colorCode = brightGreenColor
+	}
+
+	if today {
+		colorCode = highlightColor
+	}
+
+	// Print the colored cell
+	fmt.Printf("%s  %s", colorCode, resetColor)
 }
--- main.go
+++ main.go
@@ -12,14 +12,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Welcome to GoGitGraph!, your chosen path: ", config.BasePath)
+	fmt.Printf("Welcome to GoGitGraph!, your chosen path: %s\n\n\n", config.BasePath)
 
 	//res := GitFolderScanner{}.Scan(args.BasePath)
 
 	//Cacher{}.Create(res)
 
 	contributionStats := ContributionStats{gitConfig: &config.GitConfig, stats: map[string]int{}}.Calculate()
-	fmt.Println("calculated contributionStats: ", contributionStats)
 
 	contributionChart := ContributionChart{Data: contributionStats, Year: 2024}
 	contributionChart.Render()
